fe8410ec52a16d6073896593dc70a3dd0d1e8e8d
Katerynaa91 <157005316+Katerynaa91@users.noreply.github.com>
2025-09-15 23:23:05 +0300

    Task 2 main folder with dags

---
+++ dags/bronze_to_silver.py
@@ -0,0 +1,35 @@
+# 2. Файл bronze_to_silver.py.
+# - Зчитує таблицю bronze
+# - Очистка тексту для всіх текстових колонок
+# - Дедублікація рядків
+# - Запис таблиці в папку silver/{table}, де {table} — ім’я таблиці.
+
+import re
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType
+from pyspark.sql.functions import col
+
+def clean_text(text):
+    return re.sub(r'[^a-zA-Z0-9,.\\"\']', '', str(text))
+
+clean_text_udf = udf(clean_text, StringType())
+
+spark = SparkSession.builder.appName("Bronze_to_Silver").getOrCreate()
+
+tables = ["athlete_bio", "athlete_event_results"]
+
+for table in tables:
+    df = spark.read.parquet(f"{os.getcwd()}/out_tables/bronze/{table}")
+    text_columns = [field.name for field in df.schema.fields if str(field.dataType) == "StringType"]
+    for col_name in text_columns:
+        df = df.withColumn(col_name, clean_text_udf(col(col_name)))
+    df = df.dropDuplicates()
+    output_path = f"{os.getcwd()}/out_tables/silver/{table}"
+    os.makedirs(output_path, exist_ok=True)
+    df.write.mode("overwrite").parquet(output_path)
+    df = spark.read.parquet(output_path)
+    df.show(truncate=False)
+
+spark.stop()
