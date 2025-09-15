3d05ac6105739b5d5c45940aa62b817b96d4d898
Katerynaa91 <157005316+Katerynaa91@users.noreply.github.com>
2025-09-15 23:26:09 +0300

    Configs

---
+++ Task1/configs.py
@@ -0,0 +1,13 @@
+db_configs = {
+    "url": "jdbc:mysql://217.61.57.46:3306/olympic_dataset",
+    "user": "neo_data_admin",
+    "password": "Proyahaxuqithab9oplp",
+    "driver": "com.mysql.cj.jdbc.Driver"
+}
+kafka_configs = {
+    "bootstrap_servers": ['77.81.230.104:9092'],
+    "username": 'admin',
+    "password": 'VawEzo1ikLtrA8Ug8THa',
+    "security_protocol": 'SASL_PLAINTEXT',
+    "sasl_mechanism": 'PLAIN'
+}
